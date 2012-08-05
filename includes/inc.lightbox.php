17ca447c9df0c5d4434a979f21c7b4babb736a22
Jonathan Torres <jonathantorres41@gmail.com>
2012-08-05 17:04:01 -0400

    Sharing lightbox CSS and copy changes.

--- includes/inc.lightbox.php
+++ includes/inc.lightbox.php
@@ -17,32 +17,35 @@
 	
 	<!-- The Steps Lightbox -->
 	<div class="sharebg">
-		<a class="closebtn" href="#">Close</a>
-		<ol class="sharenav">
-			<li id="invite" class="selected">Invitar amigos</li>
-			<li id="fbwall">Compartir en mi muro</li>
-			<li id="twitter">Compartir en Twitter</li>
-		</ol>
+		<h1 class="lightheader">
+			<a class="closebtn" href="#">Close</a>
+			<ol class="sharenav">
+				<li id="invite" class="selected">Invitar amigos</li>
+				<li id="fbwall">Publicar en mi muro</li>
+				<li id="twitter">Compartir en Twitter</li>
+			</ol>
+		</h1>
+		
 		<div class="sharecontent gradient">
 			<div id="invite_friends" class="invitations active">
-				<span class="points biggest">+1</span>
+				<span class="points biggest">+30</span>
 				<h3>Gracias por invitar a tus amigos</h3>
 				<p>Recuerda que puedes ganar más posibilidades, al compartir el concurso en Twitter y Facebook. No dejes de aumentar tus posibilidades de ganar.</p>
-				<a class="blue_btn gradient" href="#">Compartir en mi muro</a>
+				<a class="blue_btn gradient" href="#">Publicar en mi muro</a>
 				<a class="skip" href="#">Saltar este paso</a>
 			</div><!-- #invite_friends -->
 			
 			<div id="share_fb" class="invitations">
-				<span class="points biggest">+10</span>
-				<h3>Compartir en mi muro<abbr>*</abbr></h3>
-				<p>Comparte en tu muro un mensaje de invitación al concurso y acumula 10 oportunidades adicionales de ganar.</p>
-				<a class="blue_btn gradient" href="#">Compartir en mi muro</a>
+				<span class="points biggest">+25</span>
+				<h3>Publicar en mi muro</h3>
+				<p>Comparte en tu muro un mensaje de invitación al concurso y acumula 25 oportunidades adicionales de ganar.</p>
+				<a class="blue_btn gradient" href="#">Publicar en mi muro</a>
 				<a class="skip" href="#">Saltar este paso</a>
 			</div><!-- #share_fb -->
 			
 			<div id="share_twitter" class="invitations">
 				<span class="points biggest">+10</span>
-				<h3>Compartir en Twitter<abbr>*</abbr></h3>
+				<h3>Compartir en Twitter</h3>
 				<p>Recuerda que puedes ganar participaciones extras, al compartir el concurso en Twitter. Envia un tweet y aumenta tus posibilidades.</p>
 				<a class="blue_btn gradient" href="#">Compartir en Twitter</a>
 			</div><!-- #share_twitter -->
